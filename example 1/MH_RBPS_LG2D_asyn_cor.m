64d2ee901e349b12e900b95c5260d5cabd5f0634
YunqiChen003 <154879268+YunqiChen003@users.noreply.github.com>
2024-03-20 12:52:43 +0800

    Update MH_RBPS_LG2D_asyn_cor.m

--- example 1/MH_RBPS_LG2D_asyn_cor.m
+++ example 1/MH_RBPS_LG2D_asyn_cor.m
@@ -3,12 +3,12 @@ function G=MH_RBPS_LG2D_asyn_cor(M,m)
 % m: model parametes;
 fprintf('RBPS_asyn_cor\n');
 G1=MH_RBBS_LG2Dasyn_cor1(M,m);
-%% MH_RBPSnew using conditional MF smoothing;
+%% MH_RBPS using conditional MF smoothing;
 G2=Conditional_MF_smoothing_asyn_cor(G1,m);
 G.time1=G1.time+G2.time;
 G.xhat1=[G1.xnhat;G2.xlhat];
 G.Xerror1=m.x-G.xhat1;
-%% MH_RBPSnew using conditional RTS smoothing;
+%% MH_RBPS using conditional RTS smoothing;
 G3=Conditional_RTS_smoothing_asyn_cor1(G1,m);
 G.time2=G1.time+G3.time;
 G.xhat2=[G1.xnhat;G3.xlhat];
